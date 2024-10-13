bed9593ea20819f870c202078c5e96d0099c57f9
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2024-10-14 00:15:34 +0200

    Tests Fixes #1

---
+++ lib/test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Widgettest extends StatefulWidget {
+  const Widgettest({Key? key}) : super(key: key);
+
+  @override
+  _WidgettestState createState() => _WidgettestState();
+
+}
+
+class _WidgettestState extends State<Widgettest> {
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+  }
+}
--- windows/singleFileExe.md
+++ windows/singleFileExe.md
@@ -1,4 +1,4 @@
 Use enigmavb_boxed.exe
 Select build/x64/runner/Release/**\***.exe
 And select recursive folder with the build/x64/runner/Release folder
-Will need VC_redist installed, but it is usually installed, maybe experiment with
+Will need VC_redist installed, but it is usually installed, maybe experiment with auto install
